54726c5968309229078fb47d1d3cf5e3a2d5c59a
NoobNobody <83828105+NoobNobody@users.noreply.github.com>
2022-01-13 23:58:24 +0100

    Add files via upload

---
+++ Lab4/Lab4/Lekcja.cs
@@ -0,0 +1,30 @@
+﻿/*
+using (var sr = new StreamReader("sample.txt"))
+{
+    var line = sr.ReadLine();
+    while (line != null)
+    {
+        Console.WriteLine(line);
+        line = sr.ReadLine();
+    }
+}
+
+//Otwieramy stream pliku sample.txt
+
+using (var sr = new StreamReader("sample.txt"))
+{
+    var line = sr.ReadToEnd();
+    Console.WriteLine(line);
+}
+
+//Nadpisuje elementy do naszego pliku
+
+
+//Dopisuje elementy do naszego pliku
+using (var sw = new StreamWriter("sample.txt", append: true))
+{
+    sw.WriteLine("LOL");
+    sw.WriteLine("xD");
+}
+*/
+
---
+++ Lab4/Lab4/Zadania.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Zadania
+    {
+        static void Main(string[] args)
+        {
+
+            // Zad1
+            using (var sw = new StreamWriter("zad1.txt", append: true)) 
+            {
+                sw.WriteLine("24141");
+            }
+
+            // Zad2
+            Console.WriteLine("Zadanie 2");
+            using (var sr = new StreamReader("zad1.txt"))
+            {
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+            }
+
+            // Zad3
+            Console.WriteLine("\n Zadanie 3");
+            using(var sr = new StreamReader("pesels.txt"))
+            {
+                int licznik_M = 0;
+                int licznik_K = 0;
+                string pesel = sr.ReadLine();
+                while(pesel != null)
+                {
+                    int number = pesel[9] - 48;
+
+                    if (number % 2 == 0)
+                    {
+                        licznik_K++;
+                    }
+                    else
+                        licznik_M++;
+
+                    pesel = sr.ReadLine(); 
+                }
+
+                Console.WriteLine("Kobiety: " + licznik_K + " Mezczyzni: " + licznik_M);
+            }
+        }
+    }
+}
+
+
